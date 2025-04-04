e996d0f75ccf90233450b10dee0a0d180e12c5b4
TanmayAggarwal27 <tanmayagg.108gmail.com>
2025-04-04 12:57:43 +0530

    auth setup done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-/// ello 
+const express = require("express")
+const app = express()
+const authRouter = require("./routes/authRoute.js")
+const mongo = require("./lib/mongo.js")
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth",authRouter)
+
+
+
+app.listen(3000,(req,res)=>{
+    console.log("hello world")
+})
---
+++ routes/authRoute.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const userData = require("../models/authSchema");
+const tokenGenerate = require("../utils/auth");
+
+// Signup
+
+
+router.post("/signup", async (req, res) => {
+    const { username, email, password,address } = req.body;
+    let user = await userData.findOne({ email });
+    let hashPass = await bcrypt.hash(password, 15);
+
+    if (user) {
+        return res.send("<h1>User already exists</h1>");
+    }
+    user = new userData({
+        username,
+        email,
+        password: hashPass,
+        address,
+    });
+    await user.save();
+    res.redirect("/login");
+});
+
+// Login
+
+
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await userData.findOne({ email });
+
+        if (!user) {
+            return res.send("User not found");
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.render("login", {
+                error: "Incorrect Email or Password",
+            });
+        }
+
+        const token = await tokenGenerate.createTokenUser(user);
+        res.cookie("token", token).redirect("/userPage");
+    } catch (error) {
+        console.log(error);
+        return res.render("/login", {
+            error: "Incorrect Email or Password",
+        });
+    }
+});
+
+// Logout
+router.post("/logout", (req, res) => {
+    res.clearCookie("token").redirect("/");
+});
+
+module.exports = router;
